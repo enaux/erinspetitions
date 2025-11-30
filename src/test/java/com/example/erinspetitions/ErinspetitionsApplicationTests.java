7c843a91f83253f7ca51d6df835b2c6eaebb995e
enaux <enaughton7@protonmail.com>
2025-11-30 15:52:14 +0000

    Initial commit - initialised project

---
+++ src/test/java/com/example/erinspetitions/ErinspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erinspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErinspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
