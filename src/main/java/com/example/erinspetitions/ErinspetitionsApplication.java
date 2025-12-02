0af2c32b1353c7b5ae37cac118a543f3c05fda3e
enaux <enaughton7@protonmail.com>
2025-12-02 23:54:15 +0000

    Fix Servlet conflict

--- src/main/java/com/example/erinspetitions/ErinspetitionsApplication.java
+++ src/main/java/com/example/erinspetitions/ErinspetitionsApplication.java
@@ -2,17 +2,9 @@ package com.example.erinspetitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-public class ErinspetitionsApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ErinspetitionsApplication.class);
-	}
+@SpringBootApplication
+public class ErinspetitionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErinspetitionsApplication.class, args);
