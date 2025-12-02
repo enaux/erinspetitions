5f2c8beafda448613a40a20a6ae87ec99cebca4e
enaux <enaughton7@protonmail.com>
2025-12-02 23:13:35 +0000

    Alternative fix - error page filter for external Tomcat compatibility

--- src/main/java/com/example/erinspetitions/ErinspetitionsApplication.java
+++ src/main/java/com/example/erinspetitions/ErinspetitionsApplication.java
@@ -2,10 +2,11 @@ package com.example.erinspetitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class ErinspetitionsApplication extends SpringBootServletInitializer {
 
 	@Override
